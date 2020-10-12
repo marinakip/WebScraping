7296b3d4d076b749f4b8ab96466f8a7337252622
marinakip <marinakipourou@hotmail.com>
2020-10-12 23:53:11 +0300

    Data cleaning from empty values completed and a new csv with geocoded addresses created

--- DataClean.py
+++ DataClean.py
@@ -2,5 +2,30 @@ import pandas as pd
 import csv
 import numpy as np
 
-df = pd.read_csv("addresses_all.csv", header = None)
-print(df)
+data = pd.read_csv("addresses_all.csv", header = None)
+#print(data)
+
+df = data.drop(data.columns[-1], axis=1)
+#print(df)
+
+# size = data.size
+# print(size)
+# size2 = df.size
+# print(size2)
+
+addresses = []
+counter = 0
+#print(df.values)
+for i in range(len(df.columns)):
+    for j in range(len(df)):
+        if (df[i].iloc[j] != 'NONE'):
+            addresses.append(df[i].iloc[j])
+            counter += 1
+
+#print("count None: ", str(counter))
+#print(addresses)
+#print(df.values.size)
+
+with open("addresses_cleaned.csv", "w", encoding="utf-8") as f:
+    w = csv.writer(f, delimiter="\n")
+    w.writerow(addresses)
--- Visualization.py
+++ Visualization.py
@@ -4,39 +4,76 @@ import pandas as pd
 from geopy import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
 import folium
+import time
+import datetime
+import random
 
 
-# df = pd.read_csv("addresses_all.csv", header = None)
-# print(df)
+df = pd.read_csv("addresses_cleaned.csv", sep='\"', header = None)
+#print(df)
 
-string = "Barkhamsted, CT"
-df = pd.DataFrame([string], columns=['string_values'])
-print(df)
-locator = Nominatim(user_agent="myGeocoder")
-geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
-#df['location'] = locator.geocode("Champ de Mars, Paris, France")
-df['location'] = df.apply(geocode)
-# 2- - create location column
-#df['location'] = df.apply(geocode)
-# 3 - create longitude, laatitude and altitude from location column (returns tuple)
-df['point'] = df['location'].apply(lambda loc: tuple(loc.point) if loc else None)
-# 4 - split point column into latitude, longitude and altitude columns
-df[['latitude', 'longitude']] = pd.DataFrame(df['point'].tolist(), index=df.index)
+geocoding_results = []
+counter = 0
 
-map1 = folium.Map(
-    location=[59.338315,18.089960],
-    tiles='cartodbpositron',
-    zoom_start=12,
-)
-df.apply(lambda row:folium.CircleMarker(location = [row["latitude"], row["longitude"]]).add_to(map1), axis=1)
-map1
+for row in range(len(df)):
+    counter += 1
+    if counter % 30 == 0:
+        print("SLEEP")
+        secs = random.randint(20, 60)
+        print("SLEEP " + str(secs))
+        time.sleep(secs)
+    print("GEOCODING LOCATION:  " + str(counter))
+    address = df.iloc[row].values
+    #print(address)
+    #print(address.to_string())
+    locator = Nominatim(user_agent="myGeocoder")
+    geocode = RateLimiter(locator.geocode, min_delay_seconds=2)
+    try:
+        location = address.apply(geocode)
+        #location = locator.geocode(address)
+        latitude = location.latitude
+        longitude = location.longitude
+    except AttributeError:
+        location = "NONE"
+        latitude = "NONE"
+        longitude = "NONE"
+    #print("Latitude = {}, Longitude = {}".format(location.latitude, location.longitude))
+    dictionary = {
+        'Address': address,
+        'Latitude': latitude,
+        'Longitude': longitude
+    }
+    geocoding_results.append(dictionary)
+
+df_addresses = pd.DataFrame(geocoding_results)
+df_addresses.to_csv('addresses_geocoded.csv')
+print("CSV GEOCODING ADDRESSES TEMP CREATED")
 
 
-
-
+#
+#
+# string =  #"Barkhamsted, CT"
+# df = pd.DataFrame([string], columns=['string_values'])
+# #print(df)
 # locator = Nominatim(user_agent="myGeocoder")
-# location = locator.geocode("Champ de Mars, Paris, France")
-# print("Latitude = {}, Longitude = {}".format(location.latitude, location.longitude))
+# geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
+# #df['location'] = locator.geocode("Champ de Mars, Paris, France")
+# df['location'] = df.apply(geocode)
+# # 2- - create location column
+# #df['location'] = df.apply(geocode)
+# # 3 - create longitude, laatitude and altitude from location column (returns tuple)
+# df['point'] = df['location'].apply(lambda loc: tuple(loc.point) if loc else None)
+# # 4 - split point column into latitude, longitude and altitude columns
+# df[['latitude', 'longitude']] = pd.DataFrame(df['point'].tolist(), index=df.index)
+#
+# map1 = folium.Map(
+#     location=[59.338315,18.089960],
+#     tiles='cartodbpositron',
+#     zoom_start=12,
+# )
+# df.apply(lambda row:folium.CircleMarker(location = [row["latitude"], row["longitude"]]).add_to(map1), axis=1)
+# map1
+#
 
 
 
