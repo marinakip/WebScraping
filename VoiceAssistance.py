bcd2e965e9d96dc5f4155d9980f99f35910afd49
marinakip <marinakipourou@hotmail.com>
2020-10-30 23:08:44 +0200

    Basic voice assistance created with a few basic commands

--- VoiceAssistance.py
+++ VoiceAssistance.py
@@ -1,74 +1,82 @@
 import speech_recognition as sr
 import pyttsx3
-import datetime
-import webbrowser
-import os
 import time
-import subprocess
-import json
-import requests
+import datetime
+
+
+def greet():
+    hour = datetime.datetime.now().hour
+    if 6 <= hour < 12:
+        speak("Good Morning Marina!")
+        print("Good Morning Marina!")
+    elif 12 <= hour < 21:
+        speak("Good Afternoon Marina!")
+        print("Good Afternoon Marina!")
+    else:
+        speak("Good Evening Marina!")
+        print("Good Evening Marina!")
+
+
+def take_command():
+    recognizer = sr.Recognizer()
+    recognizer.adjust_for_ambient_noise
+    microphone = sr.Microphone()
+    with microphone as source:
+        print("Please speak...")
+        audio = recognizer.listen(source)
+        try:
+            #statement = recognizer.recognize_google(audio, language='el-GR')
+            statement = recognizer.recognize_google(audio, language='en-US')
+            print(f"You said: {statement} \n")
+
+        except Exception as e:
+            speak("Please repeat")
+            print("Please repeat")
+            return "None"
+        return statement
+
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
 
-def wishMe():
-    hour=datetime.datetime.now().hour
-    if hour>=0 and hour<12:
-        speak("Hello,Good Morning")
-        print("Hello,Good Morning")
-    elif hour>=12 and hour<18:
-        speak("Hello,Good Afternoon")
-        print("Hello,Good Afternoon")
-    else:
-        speak("Hello,Good Evening")
-        print("Hello,Good Evening")
-
-
-def takeCommand():
-    r=sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Listening...")
-        audio=r.listen(source)
-
-        try:
-            statement=r.recognize_google(audio,language='en-in')
-            print(f"user said:{statement}\n")
-
-        except Exception as e:
-            speak("Pardon me, please say that again")
-            return "None"
-        return statement
-
-
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', 'voices[1].id')
 
-print("Loading your AI personal assistant G-One")
-speak("Loading your AI personal assistant G-One")
-wishMe()
+greet()
 
-if __name__=='__main__':
+print('''Welcome, I am your personal assistant. I can give you data analysis results and geo-visualization.
+       Please say options for available commands''')
+speak('''Welcome, I am your personal assistant. I can give you data analysis results and geo-visualization.
+       Please say options for available commands''')
+
+
+if __name__ == '__main__':
 
     while True:
-        speak("Tell me how can I help you now?")
-        statement = takeCommand().lower()
-        if statement==0:
+        command = take_command().lower()
+        if command == 0:
             continue
-        if "good bye" in statement or "ok bye" in statement or "stop" in statement:
-            speak('your personal assistant G-one is shutting down,Good bye')
-            print('your personal assistant G-one is shutting down,Good bye')
+        if "options" in command:
+            speak("You can select keywords for GitHub repositories,  the time frame, "
+                  "the type of map for the results and export them to csv format")
+        elif "exit" in command or "No bye" in command or "No thanks" in command:
+            speak('Ok, exiting, Good bye')
             break
-        elif 'time' in statement:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"the time is {strTime}")
-        elif 'who are you' in statement or 'what can you do' in statement:
-            speak('I am G-one your personal assistant. I can give you data analysis results and geo-visualization')
-        elif "who made you" in statement or "who created you" in statement or "who discovered you" in statement:
-            speak("I was built by Marina")
-            print("I was built by Marina")
+        elif "keywords" in command or "Github" in command:
+            speak('Available keywords for GitHub repositories are:'
+                  'Diabetes, Machine learning')
+        elif "map" in command or "map type" in command:
+            speak('Available map types are:'
+                  'Marks clustering, heatmap')
+        elif "time" in command or "time frame" in command:
+            speak('Available time frames are '
+                  'Specific dates,  range of dates')
+        elif "specific" in command or "specific dates" in command:
+            speak('Please give a specific date')
+        #speak("Do you need something else?")
 time.sleep(5)
 
 
