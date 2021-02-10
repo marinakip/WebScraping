d5dfb38d32840552f03c8a3be7d90cfeca6c7f1b
marinakip <marinakipourou@hotmail.com>
2021-02-10 02:06:01 +0200

    Main Dashboard Search field, Category, basic Graphs

--- MainDashboard.py
+++ MainDashboard.py
@@ -10,7 +10,7 @@ import numpy as np
 # bootstrap =  "https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/solar/bootstrap.min.css"
 # app = dash.Dash(external_stylesheets=[bootstrap])
 
-features = ['Followers', 'Following', 'Stars', 'Contributions']
+categories = ['Followers', 'Following', 'Stars', 'Contributions']
 continents = ['Africa', 'Asia', 'Europe', 'North America', 'Oceania', 'South America']
 
 app = dash.Dash(__name__, title = 'GitHub\'s Users Location', external_stylesheets = [dbc.themes.SOLAR])
@@ -18,23 +18,24 @@ app = dash.Dash(__name__, title = 'GitHub\'s Users Location', external_styleshee
 app.layout = html.Div([
     html.H1(" Locations of GitHub Users", style = {'text-align': 'center'}),
     html.Div([
-        html.Div(dcc.Input(id = 'input-box', type = 'text', className = 'form-control mr-sm-2'),
-                                                                                style ={'width': '80%', 'float': 'left'}),
-        html.Button('Search', id = 'button', className = 'btn btn-secondary my-2 my-sm-0'),
+        html.Div(dcc.Input(id = 'input-box', type = 'text', className = 'form-control mr-sm-2',
+                    placeholder = 'Search for repositories'), style ={'width': '80%',
+                                                                      'display': 'inline-block'}),
+        html.Div(
+            html.Button('Search', id = 'button',  className = 'btn btn-secondary my-2 my-sm-0'),
+                                style = {'display': 'inline-block', 'width': '18%', 'float': 'center',
+                                         'padding-left': '4px', 'padding-bottom': '2px'}
+        )
+
     ]),  #DIV SEARCH
-    # html.Code([
-    #     <form class="form-inline my-2 my-lg-0">
-    #           <input class="form-control mr-sm-2" type="text" placeholder="Search">
-    #           <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-    #     </form>
-    # ]), #search2
+
     html.Div([
         html.Div([
             html.Div([
-                html.Label('Category'),
+                html.Label('Cluster by Category'),
                 dcc.Dropdown(
-                    id = 'filter-feature',
-                    options = [{'label': i, 'value': i} for i in features],
+                    id = 'filter-category',
+                    options = [{'label': i, 'value': i} for i in categories],
                     value = 'Followers',
                     clearable = False)
 
@@ -43,7 +44,7 @@ app.layout = html.Div([
         ], style = {'width': '45%', 'font-size': '20px', 'float': 'right', 'display': 'inline-block'}), #DIV CATEGORY
 
         html.Div([
-            html.Label('Continent'),
+            html.Label('Cluster by Continent'),
             dcc.Dropdown(
                 id = 'filter-continent',
                 options = [{'label': i, 'value': i} for i in continents],
@@ -72,8 +73,18 @@ app.layout = html.Div([
         )
 
     ], style = {'width': '100%', 'height': '90%', 'display': 'inline-block', 'padding': '0 20'}) #DIV GRAPHS
+    @app.callback(
+        dash.dependencies.Output('world-map', 'figure'),
+        [
+            dash.dependencies.Input('input-box', 'value'),
+            dash.dependencies.Input('filter-category', 'value'),
+            dash.dependencies.Input('filter-continent', 'value')
+        ]
+    )
 
 ])
+
+
 
 if __name__ == "__main__":
     app.run_server(debug = True)
